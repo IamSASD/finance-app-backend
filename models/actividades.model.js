2e7c4f582f908a1f894297697435ff266f978b2a
Sebastian Segura <sebastiansegdiaz@gmail.com>
2022-11-27 18:46:06 -0500

    cambios en modelo de actividad y optimizacion del controller de actividad

--- models/actividades.model.js
+++ models/actividades.model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ActividadesSchema = new Schema({
-    concepto:{type: String, required: true, max:100},
-    cantidad:{type: Number, required: true, max:100},
-    producto:{type: String, required: true, max:20},
+    concepto:{type: String, required: true},
+    cantidad:{type: Number, required: true},
+    producto:{type: String, required: true},
     ingreso_egreso:{type: String, required: true, max:80},
     userId: {type: String}
 });
--- routes/actividades.router.js
+++ routes/actividades.router.js
@@ -4,8 +4,5 @@ const actividadesController = require("../controllers/actividades.controller")
 //crud
 router.post("/", actividadesController.create)
 router.get("/", actividadesController.find)
-router.get("/:id", actividadesController.findOne)
-router.put("/:id", actividadesController.update)
-router.delete("/:id", actividadesController.remove)
 
 module.exports = router
